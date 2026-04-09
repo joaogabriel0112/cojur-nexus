8e3f0e024efaad2147e2b3a5683a3a2b26ea15b4
joaogabriel0112 <joaogabriel0112@gmail.com>
2026-04-09 12:03:59 -0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'cojur-nexus-v1';
+self.addEventListener('install', function(e){
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(e){
+  self.clients.claim();
+});
+self.addEventListener('fetch', function(e){
+  if(e.request.method !== 'GET') return;
+  e.respondWith(fetch(e.request).catch(function(){
+    return caches.match(e.request);
+  }));
+});
