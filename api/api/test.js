d41cf0325a20901ab7e9261d4ac9abfd91998ecc
joaogabriel0112 <joaogabriel0112@gmail.com>
2026-04-09 12:32:46 -0300

    Delete api/api/test.js

--- api/api/test.js
+++
@@ -1,9 +0,0 @@
-module.exports = function handler(req, res) {
-  const hasKey = !!process.env.OPENAI_API_KEY;
-  res.status(200).json({ 
-    status: 'ok', 
-    message: 'API funcionando',
-    hasOpenAIKey: hasKey,
-    keyPrefix: hasKey ? process.env.OPENAI_API_KEY.substring(0, 10) + '...' : 'NAO CONFIGURADA'
-  });
-};
